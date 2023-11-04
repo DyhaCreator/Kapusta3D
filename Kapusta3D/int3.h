5b01c093317c9a3d8cf7a5d7e323601784779ad4
DyhaCreator <andropet228@gmail.com>
2023-11-04 14:42:19 -0400

    Create int3

---
+++ Kapusta3D/int3.h
@@ -0,0 +1,15 @@
+class int3{
+    int x;
+    int y;
+    int z;
+    int3() {
+        this->x = 0;
+        this->y = 0;
+        this->z = 0;
+    }
+    int3(int x, int y, int z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+};
