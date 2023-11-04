001cf52aa6aa37a1b97345eb601377ec4d7040ee
DyhaCreator <andropet228@gmail.com>
2023-11-04 14:58:23 -0400

    Update int3

--- Kapusta3D/structs/int3.hpp
+++ Kapusta3D/structs/int3.hpp
@@ -7,9 +7,42 @@ struct int3{
         this->y = 0;
         this->z = 0;
     }
+    int3(int n) {
+        this->x = n;
+        this->y = n;
+        this->z = n;
+    }
     int3(int x, int y, int z) {
         this->x = x;
         this->y = y;
         this->z = z;
+    }
+    int3 operator+(const int3 &a) {
+        int3 c = int3();
+        c.x = this->x + a.x;
+        c.y = this->y + a.y;
+        c.z = this->z + a.z;
+        return c;
+    }
+    int3 operator-(const int3 &a) {
+        int3 c = int3();
+        c.x = this->x - a.x;
+        c.y = this->y - a.y;
+        c.z = this->z - a.z;
+        return c;
+    }
+    int3 operator*(const int3 &a) {
+        int3 c = int3();
+        c.x = this->x * a.x;
+        c.y = this->y * a.y;
+        c.z = this->z * a.z;
+        return c;
+    }
+    int3 operator/(const int3 &a) {
+        int3 c = int3();
+        c.x = this->x / a.x;
+        c.y = this->y / a.y;
+        c.z = this->z / a.z;
+        return c;
     }
 };
