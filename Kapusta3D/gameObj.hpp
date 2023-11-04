9d7d6fd9da1dd21d8e77f63fe34b86080472362a
DyhaCreator <andropet228@gmail.com>
2023-11-04 15:41:05 -0400

    Update model and add model to GameObj

--- Kapusta3D/gameObj.hpp
+++ Kapusta3D/gameObj.hpp
@@ -1,7 +1,15 @@
 class GameObj{
     int3 pos;
+    Model model;
     GameObj() {}
     GameObj(int3 pos) {
         this->pos = pos;
+    }
+    GameObj(Model model) {
+        this->model = model;
+    }
+    GameObj(int3 pos, Model model) {
+        this->pos = pos;
+        this->model = model;
     }
 };
--- Kapusta3D/structs/model.hpp
+++ Kapusta3D/structs/model.hpp
@@ -1,3 +1,5 @@
 struct Model{
     std::vector<float3>points = std::vector<float3>();
+    std::vector<Edge>edges = std::vector<Edge>();
+    std::vector<Triangle>triangles = std::vector<Triangle>();
 };
