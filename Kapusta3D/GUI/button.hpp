b809866e2ffc9b9e6e4f49b07e7ddf70604284b4
DyhaCreator <andropet228@gmail.com>
2023-12-03 17:33:51 +0500

    clickable

--- Kapusta3D/GUI/button.hpp
+++ Kapusta3D/GUI/button.hpp
@@ -1,7 +1,11 @@
-struct Button{
-    int2 pos;
-    int2 size;
+class Button{
+public:
+    int2 pos = int2(10, 10);
+    int2 size = int2(200, 100);
     color def = color(200, 200, 200), hover = color(220, 220, 220), pressed = color(255, 255, 255);
+    bool isPointed = false;
+    bool isClick = false;
+    void (*isClicked) ();
     Button() {}
     Button(int2 pos) {
         this->pos = pos;
@@ -18,6 +22,9 @@ struct Button{
         this->pos = int2(x, y);
         this->size = int2(sx, sy);
     }
+    Button(void (*func)()) {
+        this->isClicked = func;
+    }
     Button& setPosition(int2 pos) {
         this->pos = pos;
         return *this;
@@ -32,6 +39,29 @@ struct Button{
             }
         } else {
             graph.drawRect(pos, size, def);
+        }
+        update();
+    }
+private:
+    bool prevState = false;
+    void update() {
+        if (MouseX > this->pos.x && MouseX < this->pos.x + this->size.x &&
+            MouseY > this->pos.y && MouseY < this->pos.y + this->size.y) {
+            isPointed = true;
+            if (MousePressed) {
+                isClick = true;
+                if (prevState == false) {
+                    prevState = true;
+                    isClicked();
+                }
+            } else {
+                prevState = false;
+                isClick = false;
+            }
+        } else {
+            isPointed = false;
+            isClick = false;
+            prevState = false;
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include "Kapusta3D/engine.hpp"
 using namespace std;
 
+void b1IsClicked() {
+    std::cout << "b1" << std::endl;
+}
+
 int main() {
     cout << "Hello, World!" << endl;
     Engine eng = Engine();
 
-    Button b1 = Button(int2(20, 20), int2(200, 100));
+    Button b1 = Button(&b1IsClicked, int2(20, 20), int2(200, 100));
     while (eng.isStarted) {
         eng.update();
         b1.show();
